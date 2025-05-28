e6f1f3d39e5c6c384a82349de9c334170f824423
Sakarrex <sebasgarciafontana@gmail.com>
2025-05-28 18:42:03 -0300

    Mejorada timeline

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Crear 5 pacientes
-        Paciente::factory(20)->create()->each(function ($paciente) {
+        Paciente::factory(1)->create()->each(function ($paciente) {
             // Para cada paciente, crear 2 hitos
-            Hito::factory(10)->create(['id_paciente' => $paciente->id_paciente])->each(function ($hito) {
+            Hito::factory(40)->create(['id_paciente' => $paciente->id_paciente])->each(function ($hito) {
                 // Para cada hito, crear 3 mediciones
                 Medicion::factory(5)->create(['id_hito' => $hito->id_hito])->each(function ($medicion) {
                     // Para cada medición, crear 5 repeticiones
